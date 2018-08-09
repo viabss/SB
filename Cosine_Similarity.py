1087e972c9e17b8de1da24f701da5aa77adeed8b
Saurav Bhattacharyya <viabss@gmail.com>
2018-08-09 01:31:14 -0400

    First Commit

---
+++ Cosine_Similarity.py
@@ -0,0 +1,13 @@
+import argparse as ap
+
+def cosine_sim(strings):
+    for
+
+def main():
+    parser = ap.ArgumentParser()
+    parser.add_argument('-s', '--string', nargs=2)
+    args = parser.parse_args()
+    cosine_sim(args.string)
+
+if __name__ == '__main__':
+    main()
